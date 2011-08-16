f23a9bc8c55df9b0498b58d6823f0b57ca80641b
Ted Naleid <contact@naleid.com>
2011-08-15 21:03:48 -0500

    incrementing version number for release

--- RedisGrailsPlugin.groovy
+++ RedisGrailsPlugin.groovy
@@ -18,14 +18,13 @@ import redis.clients.jedis.JedisPoolConfig
 import redis.clients.jedis.Protocol
 
 class RedisGrailsPlugin {
-    def version = "1.0.0.M7"
+    def version = "1.0.0.M8"
     def grailsVersion = "1.3.4 > *"
     def author = "Ted Naleid"
     def authorEmail = "contact@naleid.com"
     def title = "Redis Plugin"
 
-    def description = '''\
-    The Redis plugin provides integration with a Redis datastore. Redis is a lightning fast 'data structure server'.  The plugin enables a number of memoization techniques to cache results from complex operations in Redis.
+    def description = '''The Redis plugin provides integration with a Redis datastore. Redis is a lightning fast 'data structure server'.  The plugin enables a number of memoization techniques to cache results from complex operations in Redis.
 '''
 
     def license = "APACHE"
