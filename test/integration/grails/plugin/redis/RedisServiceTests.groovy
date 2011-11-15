fa26d90c45e6525a63adca738aea82519ee469d3
Brian Coles <stlcubsfan@gmail.com>
2011-11-15 08:58:52 -0600

    adding set based integration tests

--- test/integration/grails/plugin/redis/RedisServiceTests.groovy
+++ test/integration/grails/plugin/redis/RedisServiceTests.groovy
@@ -114,6 +114,40 @@ class RedisServiceTests extends GroovyTestCase {
         assertEquals expectedHash, result
         assertTrue NO_EXPIRATION_TTL < redisService.ttl("mykey")
     }
+    
+    def testMemoizeSet() {
+		def book1 = "book1"
+		def book2 = "book2"
+		def book3 = "book3"
+		def bookSet = [book1, book2, book3] as Set
+
+		def calledCount = 0
+		def cacheMissClosure = {
+			calledCount += 1
+			return bookSet
+		}
+
+		def cacheMissList = redisService.memoizeSet("Books", cacheMissClosure)
+
+		assertEquals 1, calledCount
+		assertEquals([book1, book2, book3] as Set, cacheMissList)
+		assertEquals NO_EXPIRATION_TTL, redisService.ttl("Books")
+
+		def cacheHitList = redisService.memoizeSet("Books", cacheMissClosure)
+
+		// cache hit, don't call closure again
+		assertEquals 1, calledCount
+		assertEquals([book1, book2, book3] as Set, cacheHitList)
+		assertEquals cacheMissList, cacheHitList
+	}
+
+	def testMemoizeSetWithExpire() {
+		def book1 = "book1"
+		assertEquals NO_EXPIRATION_TTL, redisService.ttl("Books")
+		def result = redisService.memoizeSet("Books", 60) { [book1] as Set }
+		assertEquals([book1] as Set, result)
+		assertTrue NO_EXPIRATION_TTL < redisService.ttl("Books")
+	}    
 
     def testWithTransaction() {
         redisService.withRedis { Jedis redis ->
