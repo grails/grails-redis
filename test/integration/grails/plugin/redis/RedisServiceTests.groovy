037355e79f5410d649a62ad55abfa1608de4e030
stlcubsfan <stlcubsfan@gmail.com>
2011-11-15 09:37:06 -0600

    still working on fixing sts whitespace

--- test/integration/grails/plugin/redis/RedisServiceTests.groovy
+++ test/integration/grails/plugin/redis/RedisServiceTests.groovy
@@ -5,194 +5,195 @@ import redis.clients.jedis.Jedis
 import redis.clients.jedis.Transaction
 
 class RedisServiceTests extends GroovyTestCase {
-	def redisService
+    def redisService
 
-	boolean transactional = false
+    boolean transactional = false
 
-	protected void setUp() {
-		super.setUp()
-		redisService.flushDB()
-	}
+    protected void setUp() {
+        super.setUp()
+        redisService.flushDB()
+    }
 
-	void testFlushDB() {
-		// actually called as part of setup too, but we can test it here
-		redisService.withRedis { Jedis redis ->
-			assertEquals 0, redis.dbSize()
-			redis.set("foo", "bar")
-			assertEquals 1, redis.dbSize()
-		}
+    void testFlushDB() {
+        // actually called as part of setup too, but we can test it here
+        redisService.withRedis { Jedis redis ->
+            assertEquals 0, redis.dbSize()
+            redis.set("foo", "bar")
+            assertEquals 1, redis.dbSize()
+        }
 
-		redisService.flushDB()
+        redisService.flushDB()
 
-		redisService.withRedis { Jedis redis ->
-			assertEquals 0, redis.dbSize()
-		}
-	}
+        redisService.withRedis { Jedis redis ->
+            assertEquals 0, redis.dbSize()
+        }
+    }
 
-	void testMemoizeKey() {
-		def calledCount = 0
-		def cacheMissClosure = {
-			calledCount += 1
-			return "foo"
-		}
-		def cacheMissResult = redisService.memoize("mykey", cacheMissClosure)
+    void testMemoizeKey() {
+        def calledCount = 0
+        def cacheMissClosure = {
+            calledCount += 1
+            return "foo"
+        }
+        def cacheMissResult = redisService.memoize("mykey", cacheMissClosure)
 
-		assertEquals 1, calledCount
-		assertEquals "foo", cacheMissResult
-		assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
+        assertEquals 1, calledCount
+        assertEquals "foo", cacheMissResult
+        assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
 
-		def cacheHitResult = redisService.memoize("mykey", cacheMissClosure)
+        def cacheHitResult = redisService.memoize("mykey", cacheMissClosure)
 
-		// should have hit the cache, not called our method again
-		assertEquals 1, calledCount
-		assertEquals "foo", cacheHitResult
-	}
+        // should have hit the cache, not called our method again
+        assertEquals 1, calledCount
+        assertEquals "foo", cacheHitResult
+    }
 
-	void testMemoizeKeyWithExpire() {
-		assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
-		def result = redisService.memoize("mykey", 60) { "foo" }
-		assertEquals "foo", result
-		assertTrue NO_EXPIRATION_TTL < redisService.ttl("mykey")
-	}
+    void testMemoizeKeyWithExpire() {
+        assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
+        def result = redisService.memoize("mykey", 60) { "foo" }
+        assertEquals "foo", result
+        assertTrue NO_EXPIRATION_TTL < redisService.ttl("mykey")
+    }
 
-	void testMemoizeHashField() {
-		def calledCount = 0
-		def cacheMissClosure = {
-			calledCount += 1
-			return "foo"
-		}
-		def cacheMissResult = redisService.memoizeHashField("mykey", "first", cacheMissClosure)
+    void testMemoizeHashField() {
+        def calledCount = 0
+        def cacheMissClosure = {
+            calledCount += 1
+            return "foo"
+        }
+        def cacheMissResult = redisService.memoizeHashField("mykey", "first", cacheMissClosure)
 
-		assertEquals 1, calledCount
-		assertEquals "foo", cacheMissResult
-		assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
+        assertEquals 1, calledCount
+        assertEquals "foo", cacheMissResult
+        assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
 
-		def cacheHitResult = redisService.memoizeHashField("mykey", "first", cacheMissClosure)
+        def cacheHitResult = redisService.memoizeHashField("mykey", "first", cacheMissClosure)
 
-		// should have hit the cache, not called our method again
-		assertEquals 1, calledCount
-		assertEquals "foo", cacheHitResult
+        // should have hit the cache, not called our method again
+        assertEquals 1, calledCount
+        assertEquals "foo", cacheHitResult
 
-		def cacheMissSecondResult = redisService.memoizeHashField("mykey", "second", cacheMissClosure)
+        def cacheMissSecondResult = redisService.memoizeHashField("mykey", "second", cacheMissClosure)
 
-		// cache miss because we're using a different field in the same key
-		assertEquals 2, calledCount
-		assertEquals "foo", cacheMissSecondResult
-	}
+        // cache miss because we're using a different field in the same key
+        assertEquals 2, calledCount
+        assertEquals "foo", cacheMissSecondResult
+    }
 
-	void testMemoizeHashFieldWithExpire() {
-		assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
-		def result = redisService.memoizeHashField("mykey", "first", 60) { "foo" }
-		assertEquals "foo", result
-		assertTrue NO_EXPIRATION_TTL < redisService.ttl("mykey")
-	}
+    void testMemoizeHashFieldWithExpire() {
+        assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
+        def result = redisService.memoizeHashField("mykey", "first", 60) { "foo" }
+        assertEquals "foo", result
+        assertTrue NO_EXPIRATION_TTL < redisService.ttl("mykey")
+    }
 
-	def testMemoizeHash() {
-		def calledCount = 0
-		def expectedHash = [foo: 'bar', baz: 'qux']
-		def cacheMissClosure = {
-			calledCount += 1
-			return expectedHash
-		}
-		def cacheMissResult = redisService.memoizeHash("mykey", cacheMissClosure)
+    def testMemoizeHash() {
+        def calledCount = 0
+        def expectedHash = [foo: 'bar', baz: 'qux']
+        def cacheMissClosure = {
+            calledCount += 1
+            return expectedHash
+        }
+        def cacheMissResult = redisService.memoizeHash("mykey", cacheMissClosure)
 
-		assertEquals 1, calledCount
-		assertEquals expectedHash, cacheMissResult
-		assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
+        assertEquals 1, calledCount
+        assertEquals expectedHash, cacheMissResult
+        assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
 
-		def cacheHitResult = redisService.memoizeHash("mykey", cacheMissClosure)
+        def cacheHitResult = redisService.memoizeHash("mykey", cacheMissClosure)
 
-		// should have hit the cache, not called our method again
-		assertEquals 1, calledCount
-		assertEquals expectedHash, cacheHitResult
-	}
+        // should have hit the cache, not called our method again
+        assertEquals 1, calledCount
+        assertEquals expectedHash, cacheHitResult
+    }
 
-	void testMemoizeHashWithExpire() {
-		def expectedHash = [foo: 'bar', baz: 'qux']
-		assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
-		def result = redisService.memoizeHash("mykey", 60) { expectedHash }
-		assertEquals expectedHash, result
-		assertTrue NO_EXPIRATION_TTL < redisService.ttl("mykey")
-	}
+    void testMemoizeHashWithExpire() {
+        def expectedHash = [foo: 'bar', baz: 'qux']
+        assertEquals NO_EXPIRATION_TTL, redisService.ttl("mykey")
+        def result = redisService.memoizeHash("mykey", 60) { expectedHash }
+        assertEquals expectedHash, result
+        assertTrue NO_EXPIRATION_TTL < redisService.ttl("mykey")
+    }
 
-	def testMemoizeSet() {
-		def book1 = "book1"
-		def book2 = "book2"
-		def book3 = "book3"
-		def bookSet = [book1, book2, book3]as Set
+    def testMemoizeSet() {
+        def book1 = "book1"
+        def book2 = "book2"
+        def book3 = "book3"
+        def bookSet = [book1, book2, book3]as Set
 
-		def calledCount = 0
-		def cacheMissClosure = {
-			calledCount += 1
-			return bookSet
-		}
+        def calledCount = 0
+        def cacheMissClosure = {
+            calledCount += 1
+            return bookSet
+        }
 
-		def cacheMissList = redisService.memoizeSet("Books", cacheMissClosure)
+        def cacheMissList = redisService.memoizeSet("Books", cacheMissClosure)
 
-		assertEquals 1, calledCount
-		assertEquals([book1, book2, book3]as Set, cacheMissList)
-		assertEquals NO_EXPIRATION_TTL, redisService.ttl("Books")
+        assertEquals 1, calledCount
+        assertEquals([book1, book2, book3]as Set, cacheMissList)
+        assertEquals NO_EXPIRATION_TTL, redisService.ttl("Books")
 
-		def cacheHitList = redisService.memoizeSet("Books", cacheMissClosure)
+        def cacheHitList = redisService.memoizeSet("Books", cacheMissClosure)
 
-		// cache hit, don't call closure again
-		assertEquals 1, calledCount
-		assertEquals([book1, book2, book3]as Set, cacheHitList)
-		assertEquals cacheMissList, cacheHitList
-	}
+        // cache hit, don't call closure again
+        assertEquals 1, calledCount
+        assertEquals([book1, book2, book3]as Set, cacheHitList)
+        assertEquals cacheMissList, cacheHitList
+    }
 
-	def testMemoizeSetWithExpire() {
-		def book1 = "book1"
-		assertEquals NO_EXPIRATION_TTL, redisService.ttl("Books")
-		def result = redisService.memoizeSet("Books", 60) { [book1]as Set }
-		assertEquals([book1]as Set, result)
-		assertTrue NO_EXPIRATION_TTL < redisService.ttl("Books")
-	}
 
-	def testWithTransaction() {
-		redisService.withRedis { Jedis redis ->
-			assertNull redis.get("foo")
-			redisService.withTransaction { Transaction transaction ->
-				transaction.set("foo", "bar")
-				assertNull redis.get("foo")
-			}
-			assertEquals "bar", redis.get("foo")
-		}
-	}
+    def testMemoizeSetWithExpire() {
+        def book1 = "book1"
+        assertEquals NO_EXPIRATION_TTL, redisService.ttl("Books")
+        def result = redisService.memoizeSet("Books", 60) { [book1]as Set }
+        assertEquals([book1]as Set, result)
+        assertTrue NO_EXPIRATION_TTL < redisService.ttl("Books")
+    }
 
-	def testPropertyMissingGetterRetrievesStringValue() {
-		assertNull redisService.foo
+    def testWithTransaction() {
+        redisService.withRedis { Jedis redis ->
+            assertNull redis.get("foo")
+            redisService.withTransaction { Transaction transaction ->
+                transaction.set("foo", "bar")
+                assertNull redis.get("foo")
+            }
+            assertEquals "bar", redis.get("foo")
+        }
+    }
 
-		redisService.withRedis { Jedis redis ->
-			redis.set("foo", "bar")
-		}
+    def testPropertyMissingGetterRetrievesStringValue() {
+        assertNull redisService.foo
 
-		assertEquals "bar", redisService.foo
-	}
+        redisService.withRedis { Jedis redis ->
+            redis.set("foo", "bar")
+        }
 
-	def testPropertyMissingSetterSetsStringValue() {
-		redisService.withRedis { Jedis redis ->
-			assertNull redis.foo
-		}
+        assertEquals "bar", redisService.foo
+    }
 
-		redisService.foo = "bar"
+    def testPropertyMissingSetterSetsStringValue() {
+        redisService.withRedis { Jedis redis ->
+            assertNull redis.foo
+        }
 
-		redisService.withRedis { Jedis redis ->
-			assertEquals "bar", redis.foo
-		}
-	}
+        redisService.foo = "bar"
 
-	def testMethodMissingDelegatesToJedis() {
-		assertNull redisService.foo
+        redisService.withRedis { Jedis redis ->
+            assertEquals "bar", redis.foo
+        }
+    }
 
-		redisService.set("foo", "bar")
+    def testMethodMissingDelegatesToJedis() {
+        assertNull redisService.foo
 
-		assertEquals "bar", redisService.foo
-	}
+        redisService.set("foo", "bar")
 
-	def testMethodNotOnJedisThrowsMethodMissingException() {
-		def result = shouldFail { redisService.methodThatDoesNotExistAndNeverWill() }
+        assertEquals "bar", redisService.foo
+    }
 
-		assert result?.startsWith("No signature of method: redis.clients.jedis.Jedis.methodThatDoesNotExistAndNeverWill")
-	}
+    def testMethodNotOnJedisThrowsMethodMissingException() {
+        def result = shouldFail { redisService.methodThatDoesNotExistAndNeverWill() }
+
+        assert result?.startsWith("No signature of method: redis.clients.jedis.Jedis.methodThatDoesNotExistAndNeverWill")
+    }
 }
