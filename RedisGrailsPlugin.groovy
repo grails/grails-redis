fc812a0c6a69a4526d6ff35abd5b046c2cf27c39
Ted Naleid <contact@naleid.com>
2011-12-10 21:04:54 -0600

    Updating version number to prepare for release of 1.1

--- RedisGrailsPlugin.groovy
+++ RedisGrailsPlugin.groovy
@@ -18,7 +18,7 @@ import redis.clients.jedis.JedisPoolConfig
 import redis.clients.jedis.Protocol
 
 class RedisGrailsPlugin {
-    def version = "1.0.0.M9"
+    def version = "1.1"
     def grailsVersion = "1.3.4 > *"
     def author = "Ted Naleid"
     def authorEmail = "contact@naleid.com"
