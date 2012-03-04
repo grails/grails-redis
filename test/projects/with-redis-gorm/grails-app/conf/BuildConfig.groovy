1d0ded6fcd62240be3076b2ab6b7bbedde177a4d
Ted Naleid <contact@naleid.com>
2012-03-04 14:13:50 -0600

    working on getting configs stable

--- test/projects/with-redis-gorm/grails-app/conf/BuildConfig.groovy
+++ test/projects/with-redis-gorm/grails-app/conf/BuildConfig.groovy
@@ -4,27 +4,40 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-grails.plugin.location.redis = "../../.."    
 
 grails.project.dependency.resolution = {
     inherits("global") {
     }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "info" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
 
+    useOrigin true
     repositories {
+        /*
         inherits true // Whether to inherit repository definitions from plugins
         grailsPlugins()
         grailsHome()
         grailsCentral()
         mavenCentral()
+        */
+
+        mavenLocal()
     }
     dependencies {
     }
 
     plugins {
-        runtime(':redis-gorm:1.0.0.M8') {
-            excludes('redis')
-        }
+//        compile(':redis-gorm:1.0.0.M8') {
+//            excludes('redis')
+//        }
     }
 }
+
+grails.plugin.location.redis = "../../.."
+
+// need to have the grails-data-mapping installed locally for this to work
+grails.plugin.location.'redis-gorm' = "../../../../grails-data-mapping/grails-plugins/redis/"
+
+
+
+
