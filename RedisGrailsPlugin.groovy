e760ead8819b8b2aef9e2a2933cc31f2dcdcc66b
Ted Naleid <contact@naleid.com>
2014-02-16 22:07:43 -0600

    bump plugin version number

--- RedisGrailsPlugin.groovy
+++ RedisGrailsPlugin.groovy
@@ -22,7 +22,7 @@ import redis.clients.jedis.Protocol
 
 class RedisGrailsPlugin {
 
-    def version = "1.4.3"
+    def version = "1.4.4"
     def grailsVersion = "2.0.0 > *"
     def author = "Ted Naleid"
     def authorEmail = "contact@naleid.com"
