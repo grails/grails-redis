ed2c18f3d92d02b81836495a1b6351d577c4fbb8
Ted Naleid <contact@naleid.com>
2014-03-16 16:59:33 -0500

    prep for release

--- RedisGrailsPlugin.groovy
+++ RedisGrailsPlugin.groovy
@@ -22,7 +22,7 @@ import redis.clients.jedis.Protocol
 
 class RedisGrailsPlugin {
 
-    def version = "1.5.0"
+    def version = "1.5.1"
     def grailsVersion = "2.0.0 > *"
     def author = "Ted Naleid"
     def authorEmail = "contact@naleid.com"
