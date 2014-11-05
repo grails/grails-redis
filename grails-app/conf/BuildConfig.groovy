7645de9a4bfea306201654c88854f9219e51d976
Ted Naleid <contact@naleid.com>
2014-11-04 20:00:44 -0600

    upgrading to grails 2.4.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,28 +1,37 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
 
 grails.project.fork = [
         test: false
 ]
 
-grails.project.dependency.resolution = {
+grails.project.dependency.resolver = "maven"
 
+grails.project.dependency.resolution = {
     inherits "global"
     log "warn"
+    checksums true
+    legacyResolve false
+
 
     repositories {
+        inherits true
+
         grailsPlugins()
         grailsHome()
+        mavenLocal()
         grailsCentral()
+        jcenter()
         mavenCentral()
-        grailsRepo "http://grails.org/plugins"
-//        mavenRepo "http://m2repo.spockframework.org/snapshots"
+        mavenRepo "http://repo.spring.io/milestone/"
+        mavenRepo "http://oss.sonatype.org/content/repositories/snapshots"
     }
 
     dependencies {
-	    compile 'redis.clients:jedis:2.4.2'
-		compile 'com.google.code.gson:gson:2.2.4'
+	    compile 'redis.clients:jedis:2.6.0'
+  		compile 'com.google.code.gson:gson:2.2.4'
     }
 
     plugins {
