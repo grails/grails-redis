4eebe41e5f051279573b586e027a37274f4d5319
christian.oestreich <christian.oestreich@bluestembrands.com>
2015-03-13 09:11:29 -0500

    Code cleanup and removing comments

--- src/main/groovy/redis/RedisGrailsPlugin.groovy
+++ src/main/groovy/redis/RedisGrailsPlugin.groovy
@@ -34,9 +34,9 @@ class RedisGrailsPlugin extends Plugin {
 
     def developers = [
             [name: "Burt Beckwith"],
+            [name: "Christian Oestreich"],
             [name: "Brian Coles"],
             [name: "Michael Cameron"],
-            [name: "Christian Oestreich"],
             [name: "John Engelman"],
             [name: "David Seiler"],
             [name: "Jordon Saardchit"],
@@ -55,54 +55,4 @@ class RedisGrailsPlugin extends Plugin {
             }
         }
     }
-
-//    def configureServiceInline(builder, redisConfigMap, key, serviceClass){
-//        builder.with {
-//            def poolBean = "redisPoolConfig${key}"
-//            def validPoolProperties = findValidPoolPropertiesInline(null, redisConfigMap.poolConfig)
-//            "${poolBean}"(JedisPoolConfig) {
-//                // used to set arbitrary config values without calling all of them out here or requiring any of them
-//                // any property that can be set on RedisPoolConfig can be set here
-////                validPoolProperties.each { configKey, value ->
-////                    delegate.setProperty(configKey, value)
-////                }
-//            }
-//
-//            def host = redisConfigMap?.host ?: 'localhost'
-//            def port = redisConfigMap.containsKey("port") ? "${redisConfigMap.port}" as Integer : Protocol.DEFAULT_PORT
-//            def timeout = redisConfigMap.containsKey("timeout") ? "${redisConfigMap?.timeout}" as Integer : Protocol.DEFAULT_TIMEOUT
-//            def password = redisConfigMap?.password ?: null
-//            def database = redisConfigMap?.database ?: Protocol.DEFAULT_DATABASE
-//            def sentinels = redisConfigMap?.sentinels ?: null
-//            def masterName = redisConfigMap?.masterName ?: null
-//
-//            // If sentinels and a masterName is present, using different pool implementation
-//            if (sentinels && masterName) {
-//                "redisPool${key}"(JedisSentinelPool, masterName, sentinels as Set, ref(poolBean), timeout, password, database) { bean ->
-//                    bean.destroyMethod = 'destroy'
-//                }
-//            } else {
-//                "redisPool${key}"(JedisPool, ref(poolBean), host, port, timeout, password, database) { bean ->
-//                    bean.destroyMethod = 'destroy'
-//                }
-//            }
-//
-//            "redisService${key}"(serviceClass) { bean ->
-//                redisPool = ref("redisPool${key}")
-//            }
-//        }
-//    }
-//
-//    def findValidPoolPropertiesInline(log, ConfigObject properties) {
-//        def fakeJedisPoolConfig = new JedisPoolConfig()
-//        properties.findAll{ configKey, value ->
-//            try {
-//                fakeJedisPoolConfig[configKey] = value
-//                return true
-//            } catch(Exception exception) {
-//                //log.warn "Redis pool configuration parameter (${configKey}) does not exist on JedisPoolConfig or value is the wrong type"
-//                return false
-//            }
-//        }
-//    }
 }
